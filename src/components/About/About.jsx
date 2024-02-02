27483afd65bbf5dfaec78be361c95f2316d89a08
dimovv23 <ggdimovv@gmail.com>
2024-02-02 19:09:26 +0200

    Change design

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,9 @@
 import "../About/About.css";
 import profPic from "../../assets/Untitled.png";
 import Card from "../Card.jsx";
+import techImage from "../../assets/tech.png";
+import sportsImage from "../../assets/sports.png";
+import designImage from "../../assets/design.png";
 
 export default function About() {
   return (
@@ -11,19 +14,12 @@ export default function About() {
         <h3>
           I'm, a final-year student in the Faculty of Computer Systems and
           Technologies at the "Technical University of Sofia".
-          <br />
-          Here is more about me
         </h3>
       </div>
       <div id="card-container">
-        <Card
-          cardTitle={"sports"}
-          paragraph={
-            "I really do have a passion for any kind of physical activities. Whether it's hitting the gym, swimming or running. This passion of mine outside the world of coding keeps me energized and balanced, fueling my creativity."
-          }
-        />
-        <Card cardTitle={"tech"} paragraph={""} />
-        <Card cardTitle={"design"} paragraph={""} />
+        <Card image={sportsImage} />
+        <Card image={techImage} />
+        <Card image={designImage} />
       </div>
       <br />
       <br />
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,11 @@
 import "../components/About/About.css";
 
-export default function Card({ cardTitle, paragraph }) {
+export default function Card({ image }) {
   return (
     <>
-      <div id="about-card" className="flip-card">
-        <div className="flip-card-inner">
-          <div className="flip-card-front">
-            <p>{cardTitle}</p>
-          </div>
-          <div className="flip-card-back">
-            <p>{paragraph}</p>
-          </div>
-        </div>
-      </div>
+      <button id="about-card" className="flip-card">
+        <img src={image} alt="" />
+      </button>
     </>
   );
 }
